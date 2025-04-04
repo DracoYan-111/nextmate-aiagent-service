69452faaadccbe430e9d430454e1359e9d483859
Draco <yanlong2944@gmail.com>
2025-04-04 18:12:04 +0800

    feat: Add prod

--- src/ai-agent/agent-twitter/agent-twitter.service.ts
+++ src/ai-agent/agent-twitter/agent-twitter.service.ts
@@ -36,13 +36,6 @@ export class AgentTwitterService implements OnModuleInit {
 
   // 在模块初始化时自动登录 Twitter
   async onModuleInit() {
-    const configPath =
-      process.env.CONFIG_FILES || './config.json,./confidential.json';
-    console.log(
-      configPath,
-      this.twitterUsername,
-      '--------==========---------',
-    );
     const cookiesLoaded = await this.loadCookies();
     if (!cookiesLoaded) {
       await this.login(); // 如果加载失败，则执行登录
